0d1957b629db8b28919fbd51b678873be526f5c4
Herman Ceaser <ceaserbanks09@gmail.com>
2025-01-03 19:23:27 +0300

    updated edit to accept individual fields for an update

--- src/inventory.rs
+++ src/inventory.rs
@@ -10,7 +10,7 @@ pub struct Product {
 
 pub trait InventoryManagement {
     fn add_product(&mut self, product:Product);
-    fn edit_product(&mut self, product_name: &str, new_product: Product);
+    fn edit_product(&mut self, product_name: &str, description: String, price: f64, quantity: u32);
     fn delete_product(&mut self, product_name: &str);
     
 }
@@ -24,9 +24,11 @@ impl InventoryManagement for Inventory {
         self.products.insert(product.name.clone(), product);
     }
 
-    fn edit_product(&mut self, product_name: &str, new_product: Product) {
-        if self.products.contains_key(product_name) {
-            self.products.insert(product_name.to_string(), new_product);
+    fn edit_product(&mut self, product_name: &str, description: String, price: f64, quantity: u32) {
+        if let Some(product) = self.products.get_mut(product_name) {
+            product.description = description;
+            product.price = price;
+            product.quantity = quantity;
         } else {
             //Handle Product not found later
             println!("Error: Product not found")
@@ -51,3 +53,45 @@ impl Inventory {
     }
 }
 
+// Tests
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn adds_product() {
+        let mut inventory = Inventory::new();
+
+        inventory.add_product(Product {
+            name: "Test Product".to_string(),
+            description: "A product for testing".to_string(),
+            price: 100.0,
+            quantity: 20,
+        });
+
+        assert!(inventory.products.contains_key("Test Product"));
+    }
+
+    #[test]
+    fn edits_product() {
+        let mut inventory: Inventory = Inventory::new();
+        inventory.add_product(Product {
+            name: "Test Product".to_string(),
+            description: "A product for testing".to_string(),
+            price: 100.0,
+            quantity: 20,
+        });
+        inventory.edit_product("Test Product", 
+            "Updated description".to_string(),
+            150.0,
+            15,
+        );
+
+        let product = inventory.products.get("Test Product").unwrap();
+
+        assert_eq!(product.description, "Updated description");
+        assert_eq!(product.price, 150.0);
+        assert_eq!(product.quantity, 15);
+
+    }
+}
