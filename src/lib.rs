cd9ba5cec4792c87115a62a24abdbe3195b25067
Herman Ceaser <ceaserbanks09@gmail.com>
2025-01-04 10:05:19 +0300

    refactored getuser input logic into a storeConfig struct for maintainability

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,15 @@
-mod display;
+mod util;
 pub mod inventory;
+use inventory::{add_product, delete_product, edit_product, list_products};
 use inventory::Inventory;
 
 
 pub struct Store {
     pub inventory: Inventory
+}
+
+pub struct StoreConfig<'a> {
+    pub actions: Vec<(&'a str, &'a str, fn(&mut Inventory))>
 }
 
 
@@ -14,4 +19,35 @@ impl Store {
             inventory: Inventory::new(),
         }
     }
+}
+
+impl<'a> StoreConfig<'a> {
+    pub fn new() -> Self {
+        StoreConfig {
+            actions: vec![
+                ("1", "Add Product", add_product),
+                ("2", "Edit Product", edit_product),
+                ("3", "Delete Product", delete_product),
+                ("4", "List Products", list_products),
+            ]
+        }
+    }
+
+    pub fn execute(&self, choice: &str, inventory: &mut Inventory) -> bool {
+        if let Some((_, _, action)) = self.actions.iter().find(|(key, _, _)| key == &choice) {
+            action(inventory);
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn display_menu(&self) {
+        println!("\n--- Rusty Store Management ---");
+        for (key, description, _) in &self.actions {
+            println!("{}: {}", key, description);
+        }
+        println!("6: Exit");
+    }
+
 }
